27f44576e11d271e9dac5438fa00074fb8fd2f04
Carlos-linares <145226002+Carlos-linares@users.noreply.github.com>
2024-10-09 13:58:27 -0500

    Delete AgenciaVuelos/Vuelos/Booking directory
    Signed-off-by: Carlos-linares <145226002+Carlos-linares@users.noreply.github.com>

--- AgenciaVuelos/Vuelos/Booking/src/main/java/com/Booking/BookingService/repository/BookingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.Booking.BookingService.repository;
-
-import com.Booking.BookingService.Entity.Booking;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface BookingRepository extends JpaRepository<Booking, UUID> {
-}
