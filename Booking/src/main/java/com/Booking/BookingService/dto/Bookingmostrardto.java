1946485fa95ee38833498668d03ab14bcb177efb
Carlos-linares <145226002+Carlos-linares@users.noreply.github.com>
2024-10-09 13:56:57 -0500

    Delete Booking directory
    Signed-off-by: Carlos-linares <145226002+Carlos-linares@users.noreply.github.com>

--- Booking/src/main/java/com/Booking/BookingService/dto/Bookingmostrardto.java
+++
@@ -1,8 +0,0 @@
-package com.Booking.BookingService.dto;
-import java.util.UUID;
-public record Bookingmostrardto(
-        UUID id,
-        String flightNumber,
-        String passengerName
-) {
-}
